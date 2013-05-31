faed961d422e94f3420950aca60d8e41ed9ce676
CLP <clp@chris-pc.home>
2013-05-31 16:21:39 -0400

    Initial Commit

---
+++ fdtd4/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  fdtd4
+//
+//  Created by CLP on 5/31/13.
+//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
